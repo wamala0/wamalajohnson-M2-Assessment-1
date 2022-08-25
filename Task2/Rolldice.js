21d08b6f6606d25ddd826a7a123f1b39e8fe000f
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-08-25 14:49:20 +0300

    Update Rolldice.js

--- Task2/Rolldice.js
+++ Task2/Rolldice.js
@@ -3,7 +3,6 @@ function GameStart(){
     var WinningCategories = [
         "Best Enterprise Solution",
         "Best Hackhathon solution",
-        "Best Consumer Solution",
         "Best Gaming Solution",
         "Best Health Solution",
         "Best Educational Solution",
