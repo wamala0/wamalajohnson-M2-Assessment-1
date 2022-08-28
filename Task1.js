f986578720f36644d2d5b33b942b9bcb2147d5a0
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-08-28 12:39:10 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -2,11 +2,11 @@
 const names = "My name is Wamala Johnson"
 console.log(names)
 
-//Institution
+//Institution names
 const institution = "I am a student at IT varsity/MTN Business App Academy"
 console.log(institution)
 
-//Github Username
+// My Github Username
 const GithubUsername = "My Github username is Wamala"
 
 console.log(GithubUsername)
