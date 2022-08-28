008e642f66f20863e3d523e13da8e5e63f0b28f9
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-08-28 16:51:09 +0300

    Add files via upload

---
+++ TaskA.js
@@ -0,0 +1,12 @@
+//Names
+const names = "My name is Wamala Johnson"
+console.log(names)
+
+//Institution names
+const institution = "I am a student at IT varsity/MTN Business App Academy"
+console.log(institution)
+
+// My Github Username
+const GithubUsername = "My Github username is Wamala"
+
+console.log(GithubUsername)
